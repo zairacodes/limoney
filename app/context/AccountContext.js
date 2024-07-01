d6ab0309a1494d661998e82c7a543903fe21fcc2
Joao Ponte <joao_ponte@msn.com>
2024-07-01 11:44:07 +0100

    create accountContext

---
+++ app/context/AccountContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react'
+
+export const AccountContext = createContext()
+
+export const AccountProvider = ({ children }) => {
+  const [accBalance, setAccBalance] = useState(5000)
+
+  return (
+    <AccountContext.Provider value={{ accBalance, setAccBalance }}>
+      {children}
+    </AccountContext.Provider>
+  )
+}
