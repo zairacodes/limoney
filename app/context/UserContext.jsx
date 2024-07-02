9c54cbc3c86edbea75222861619fe8b1ce53af60
Joao Ponte <joao_ponte@msn.com>
2024-07-02 17:02:29 +0100

    Update the user object with firebase.

--- app/context/UserContext.jsx
+++ app/context/UserContext.jsx
@@ -1,38 +1,45 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react'
 import { doc, onSnapshot, updateDoc } from 'firebase/firestore'
-import { db } from '../src/config/firebase';
+import { db } from '../src/config/firebase'
 
-export const UserContext = createContext();
+export const UserContext = createContext()
 
-export const UserProvider = ({children}) => {
-    const [user, setUser] = useState({
-        "username": "Blalalala",
-        "personalBalance": 2000,
-        "businessBalance": 0,
-        "totalProfit": 0
-    })
-    const [loading, setLoading] = useState(true);
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    username: 'Blalalala',
+    accountBalance: 2000,
+    totalProfit: 0,
+    lemonCount: 0,
+    sugarCount: 0,
+    waterCount: 0,
+  })
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const unsub = onSnapshot(doc(db, 'users', 'vSHbPyV82Y4As0rEVWJG'), (doc) => {
-            if (doc.exists()) {
-              const data = doc.data()
-              console.log(data.username);
-              setUser({
-                username: data.username,
-                accountBalance: data.accountBalance,
-                lemonCount: data.lemonCount,
-                sugarCount: data.sugarCount,
-                waterCount: data.waterCount,
-                totalProfit: data.totalProfit
-              })
-              setLoading(false)
-            }
-        })
-        return () => unsub()
-    }, [])
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(db, 'users', 'vSHbPyV82Y4As0rEVWJG'),
+      (doc) => {
+        if (doc.exists()) {
+          const data = doc.data()
+          console.log(data.username)
+          setUser({
+            username: data.username,
+            accountBalance: data.accountBalance,
+            lemonCount: data.lemonCount,
+            sugarCount: data.sugarCount,
+            waterCount: data.waterCount,
+            totalProfit: data.totalProfit,
+          })
+          setLoading(false)
+        }
+      }
+    )
+    return () => unsub()
+  }, [])
 
-    return (<UserContext.Provider value={{user, setUser}}>
-        {children}
-    </UserContext.Provider>)
+  return (
+    <UserContext.Provider value={{ user, setUser, loading }}>
+      {children}
+    </UserContext.Provider>
+  )
 }
