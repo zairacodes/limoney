efb61784944663b1df8a62bf2be9eb3cb1b0eebc
zairacodes <zaira.nuovo@gmail.com>
2024-07-03 14:29:08 +0100

    implement makeLemonade function and button in Operation/LemonadeRecipe component

--- app/components/Operation/LemonadeRecipe.js
+++ app/components/Operation/LemonadeRecipe.js
@@ -1,13 +1,34 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
+import { Button } from "react-native-paper";
+import { UserContext } from "../../context/UserContext";
 
 const LemonadeRecipe = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const makeLemonade = () => {
+    if (user.lemonCount >= 1 && user.waterCount >= 4 && user.sugarCount >= 3) {
+      setUser((prevUser) => ({
+        ...prevUser,
+        lemonadeInStock: prevUser.lemonadeInStock + 1,
+        lemonCount: prevUser.lemonCount - 1,
+        waterCount: prevUser.waterCount - 4,
+        sugarCount: prevUser.sugarCount - 3,
+      }));
+    } else {
+      alert("You do not have enough ingredients in stock to make lemonade.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>Lemonade Recipe:</Text>
       <Text style={styles.ingredientText}>Lemon x 1</Text>
       <Text style={styles.ingredientText}>Water x 4</Text>
       <Text style={styles.ingredientText}>Sugar x 3</Text>
+      <Button style={styles.button} onPress={makeLemonade}>
+        MAKE LEMONADE
+      </Button>
     </View>
   );
 };
@@ -32,6 +53,14 @@ const styles = StyleSheet.create({
     marginTop: "5%",
     fontSize: 18,
     // color: "darkorange", // to be changed according to colours.js
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "5%",
+    borderRadius: 50,
+    borderWidth: 1,
+    backgroundColor: "darkorange", // to be changed according to colours.js
   },
 });
 
