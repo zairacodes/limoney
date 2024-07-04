56af6e52583dce7d17e645691133977c9531454b
zairacodes <zaira.nuovo@gmail.com>
2024-07-04 15:07:25 +0100

    add background to Operation page

--- app/(tabs)/Operation.jsx
+++ app/(tabs)/Operation.jsx
@@ -3,17 +3,21 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header.js";
 import StockList from "../components/Operation/StockList.js";
 import LemonadeRecipe from "../components/Operation/LemonadeRecipe.js";
-import { StyleSheet, ScrollView } from "react-native";
-import { colours } from "../utils/colours";
+import { StyleSheet, ScrollView, ImageBackground } from "react-native";
 
 const operation = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Header />
-        <StockList />
-        <LemonadeRecipe />
-      </ScrollView>
+      <ImageBackground
+        source={require("../utils/backgrounds/OperationsBackground.png")}
+        style={styles.container}
+      >
+        <ScrollView>
+          <Header />
+          <StockList />
+          <LemonadeRecipe />
+        </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
@@ -21,7 +25,6 @@ const operation = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colours.yellow,
   },
 });
 
