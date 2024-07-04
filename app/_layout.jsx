d8c89b5c6de88729e7161fcd95e4bb0e8c207755
Joao Ponte <joao_ponte@msn.com>
2024-07-04 21:43:06 +0100

    Delete no need files.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,29 +1,20 @@
-import { Stack } from "expo-router";
-import React from "react";
-import { AccountProvider } from "./context/AccountContext.js";
-import { UserProvider } from "./context/UserContext.jsx";
-import { DateProvider } from "./context/DateContext.jsx";
-import { DaysPlayedProvider } from "./context/DaysPlayedContext.jsx";
-import { SafeAreaView, StyleSheet } from "react-native";
-import Header from "./components/Header.js";
+import { Stack } from 'expo-router'
+import React from 'react'
+import { UserProvider } from './context/UserContext.jsx'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import Header from './components/Header.js'
 
 export default function RootLayout() {
   return (
     <UserProvider>
-      <DaysPlayedProvider>
-        <DateProvider>
-          <AccountProvider>
-            <SafeAreaView style={styles.container}>
-              <Header />
-              <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              </Stack>
-            </SafeAreaView>
-          </AccountProvider>
-        </DateProvider>
-      </DaysPlayedProvider>
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
     </UserProvider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -31,4 +22,4 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 20,
   },
-});
+})
