271d2ca4a7c9c4e3b045e260fcfd1ea48f6f8b39
zairacodes <zaira.nuovo@gmail.com>
2024-07-04 17:42:27 +0100

    add SafeAreaView and render Header component

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,11 @@
-import { Stack } from 'expo-router'
-import React from 'react'
-import { AccountProvider } from './context/AccountContext.js'
-import { UserProvider } from './context/UserContext.jsx'
-import { DateProvider } from './context/DateContext.jsx'
-import { DaysPlayedProvider } from './context/DaysPlayedContext.jsx'
+import { Stack } from "expo-router";
+import React from "react";
+import { AccountProvider } from "./context/AccountContext.js";
+import { UserProvider } from "./context/UserContext.jsx";
+import { DateProvider } from "./context/DateContext.jsx";
+import { DaysPlayedProvider } from "./context/DaysPlayedContext.jsx";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Header from "./components/Header.js";
 
 export default function RootLayout() {
   return (
@@ -11,12 +13,22 @@ export default function RootLayout() {
       <DaysPlayedProvider>
         <DateProvider>
           <AccountProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
+            <SafeAreaView style={styles.container}>
+              <Header />
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              </Stack>
+            </SafeAreaView>
           </AccountProvider>
         </DateProvider>
       </DaysPlayedProvider>
     </UserProvider>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+});
