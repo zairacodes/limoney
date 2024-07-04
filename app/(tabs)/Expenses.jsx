9cae42a44309df28b6d4f9ad9696092f231bd9f9
zairacodes <zaira.nuovo@gmail.com>
2024-07-04 14:49:00 +0100

    add background to Expenses page

--- app/(tabs)/Expenses.jsx
+++ app/(tabs)/Expenses.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, ImageBackground } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header.js";
@@ -8,20 +8,28 @@ const expenses = () => {
   const [rent, setRent] = useState(2000);
   const [utilities, setUtilities] = useState(500);
   return (
-    <SafeAreaView>
-      <Header />
-      <Text style={styles.text}>Expenses page</Text>
-      <ExpensesInfo
-        rent={rent}
-        setRent={setRent}
-        utilities={utilities}
-        setUtilities={setUtilities}
-      />
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("../utils/backgrounds/ExpensesBackground.png")}
+        style={styles.container}
+      >
+        <Header />
+        <Text style={styles.text}>Expenses page</Text>
+        <ExpensesInfo
+          rent={rent}
+          setRent={setRent}
+          utilities={utilities}
+          setUtilities={setUtilities}
+        />
+      </ImageBackground>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   text: {
     textAlign: "center",
     fontSize: 16,
