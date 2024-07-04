2461e1b68b6239f56f3d68efa31b164875bcaf7b
Joao Ponte <joao_ponte@msn.com>
2024-07-04 20:12:04 +0100

    remove context not needed it.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,13 @@
 import { Stack } from 'expo-router'
 import React from 'react'
-import { AccountProvider } from './context/AccountContext.js'
 import { UserProvider } from './context/UserContext.jsx'
-import { DateProvider } from './context/DateContext.jsx'
-import { DaysPlayedProvider } from './context/DaysPlayedContext.jsx'
 
 export default function RootLayout() {
   return (
     <UserProvider>
-      <DaysPlayedProvider>
-        <DateProvider>
-          <AccountProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-          </AccountProvider>
-        </DateProvider>
-      </DaysPlayedProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
     </UserProvider>
   )
 }
