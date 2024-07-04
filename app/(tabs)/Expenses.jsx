c690eebc5685116ba169c39fcca5a7a7df372456
zairacodes <zaira.nuovo@gmail.com>
2024-07-04 17:43:37 +0100

    remove SafeAreaView and Header component to prevent Header re-rendering when moving between screens

--- app/(tabs)/Expenses.jsx
+++ app/(tabs)/Expenses.jsx
@@ -1,39 +1,24 @@
-import { StyleSheet, ImageBackground } from "react-native";
+import { ImageBackground } from "react-native";
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Header from "../components/Header.js";
 import ExpensesInfo from "../components/Expenses/ExpensesInfo.js";
 
 const expenses = () => {
   const [rent, setRent] = useState(2000);
   const [utilities, setUtilities] = useState(500);
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        source={require("../utils/backgrounds/ExpensesBackground.png")}
-        // style={StyleSheet.absoluteFillObject}
-        resizeMode="cover"
-      >
-        <Header />
-        <ExpensesInfo
-          rent={rent}
-          setRent={setRent}
-          utilities={utilities}
-          setUtilities={setUtilities}
-        />
-      </ImageBackground>
-    </SafeAreaView>
+    <ImageBackground
+      source={require("../utils/backgrounds/ExpensesBackground.png")}
+      // style={StyleSheet.absoluteFillObject}
+      resizeMode="cover"
+    >
+      <ExpensesInfo
+        rent={rent}
+        setRent={setRent}
+        utilities={utilities}
+        setUtilities={setUtilities}
+      />
+    </ImageBackground>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 16,
-  },
-});
 
 export default expenses;
--- app/(tabs)/Information.jsx
+++ app/(tabs)/Information.jsx
@@ -6,7 +6,6 @@ import {
   ImageBackground,
 } from "react-native";
 import React, { useContext } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { colours } from "../utils/colours";
 import { UserContext } from "../context/UserContext";
 
@@ -14,60 +13,55 @@ const information = () => {
   const { user } = useContext(UserContext);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        source={require("../utils/backgrounds/InfoBackground.png")}
-        style={StyleSheet.absoluteFillObject}
-        resizeMode="cover"
-      >
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <View style={styles.box}>
-            <Text style={styles.heading}>How To Play</Text>
-            <Text style={styles.body}>
-              Congratulations {user.username}! You are the proud new owner of
-              the Limoney lemonade stand!
-            </Text>
-            <Text style={styles.body}>
-              Your aim is to earn £100,000 by selling, upgrading and investing.
-            </Text>
-            <Text style={styles.body}>Now let's go through the basics:</Text>
-            <Text style={styles.subheading}>Operation</Text>
-            <Text style={styles.body}>
-              This is where you can manage your business. See how well your
-              lemonade is selling and make sure to buy more stock so that you
-              don't run out! Running out of any ingredient means you can't make
-              lemonade - and then you can't make money!
-            </Text>
-            <Text style={styles.subheading}>Bank</Text>
-            <Text style={styles.body}>
-              The bank is where to go when you want invest. Investments allow
-              you to deposit some money and earn a percentage back after a
-              period of time. This may take a while, and a big chunk of money,
-              but will allow you to earn more in the long term.
-            </Text>
-            <Text style={styles.subheading}>Expenses</Text>
-            <Text style={styles.body}>
-              As a business owner you have some bills to pay...Rent and
-              utilities allow your stand to be open and running. If you miss a
-              payment then you will not be able to collect any more money, so
-              remember to check in regularly and pay your bills on time!
-            </Text>
-            <Text style={styles.subheading}>Info</Text>
-            <Text style={styles.body}>
-              If you ever forget what you need to do, or are not sure how
-              something works, you can find these instructions in the info tab.
-            </Text>
-          </View>
-        </ScrollView>
-      </ImageBackground>
-    </SafeAreaView>
+    <ImageBackground
+      source={require("../utils/backgrounds/InfoBackground.png")}
+      style={StyleSheet.absoluteFillObject}
+      resizeMode="cover"
+    >
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.box}>
+          <Text style={styles.heading}>How To Play</Text>
+          <Text style={styles.body}>
+            Congratulations {user.username}! You are the proud new owner of the
+            Limoney lemonade stand!
+          </Text>
+          <Text style={styles.body}>
+            Your aim is to earn £100,000 by selling, upgrading and investing.
+          </Text>
+          <Text style={styles.body}>Now let's go through the basics:</Text>
+          <Text style={styles.subheading}>Operation</Text>
+          <Text style={styles.body}>
+            This is where you can manage your business. See how well your
+            lemonade is selling and make sure to buy more stock so that you
+            don't run out! Running out of any ingredient means you can't make
+            lemonade - and then you can't make money!
+          </Text>
+          <Text style={styles.subheading}>Bank</Text>
+          <Text style={styles.body}>
+            The bank is where to go when you want invest. Investments allow you
+            to deposit some money and earn a percentage back after a period of
+            time. This may take a while, and a big chunk of money, but will
+            allow you to earn more in the long term.
+          </Text>
+          <Text style={styles.subheading}>Expenses</Text>
+          <Text style={styles.body}>
+            As a business owner you have some bills to pay...Rent and utilities
+            allow your stand to be open and running. If you miss a payment then
+            you will not be able to collect any more money, so remember to check
+            in regularly and pay your bills on time!
+          </Text>
+          <Text style={styles.subheading}>Info</Text>
+          <Text style={styles.body}>
+            If you ever forget what you need to do, or are not sure how
+            something works, you can find these instructions in the info tab.
+          </Text>
+        </View>
+      </ScrollView>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   scrollContainer: {
     flexGrow: 1,
     paddingTop: 50,
