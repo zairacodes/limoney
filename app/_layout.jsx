ac58fca2d84132c6b98bbfb88c02952db0933dfa
zairacodes <zaira.nuovo@gmail.com>
2024-07-08 15:45:49 +0100

    add stack for Winner page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,15 +8,17 @@ import Header from "./components/Header.js";
 export default function RootLayout() {
   const insets = useSafeAreaInsets();
   const segments = useSegments();
-  const isLoginRoute = segments.includes("Login");
+  const noHeaderRoute =
+    segments.includes("Login") || segments.includes("Winner");
 
   return (
     <UserProvider>
       <View style={{ paddingBottom: Math.max(insets.top, 16) }} />
-      {!isLoginRoute && <Header />}
+      {!noHeaderRoute && <Header />}
       <Stack>
         <Stack.Screen name="(tabs)/Login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)/game" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)/Winner" options={{ headerShown: false }} />
       </Stack>
     </UserProvider>
   );
